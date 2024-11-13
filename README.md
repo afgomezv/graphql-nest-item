b43c086b0097e87f3e171b15babbccb4ebd38eaf
afgomezv <afgomezvonline@gmail.com>
2024-11-13 13:53:25 -0500

    create endpoint graphql + postgres

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Development
+
+1. Clonar el proyecto
+2. copiar el ```.env.template``` y renombrar a ```.env```
+3. Ejecutar
+```
+npm install
+````
+4. Levantar la imagen docker(Docker Desktop)
+```
+docker-compose up -d
+```
+5. Ejecutar el backend de Nest
+```
+npm run start:dev
+```
+6. Visitar el sitio
+```
+localhost:3000/graphql
+```
---
+++ src/items/items.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateItemInput, UpdateItemInput } from './dto/inputs';
+import { Item } from './entities/item.entity';
+
+@Injectable()
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+  ) {}
+  async create(createItemInput: CreateItemInput): Promise<Item> {
+    const newItem = this.itemRepository.create(createItemInput);
+    return await this.itemRepository.save(newItem);
+  }
+
+  async findAll(): Promise<Item[]> {
+    // Todo: filtrar, paginar, por usuario
+    return this.itemRepository.find();
+  }
+
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item) throw new NotFoundException(`Item with id ${id} not found`);
+
+    return item;
+  }
+
+  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+    const item = await this.itemRepository.preload(updateItemInput);
+
+    if (!item) throw new NotFoundException(`Item with id ${id} not found`);
+
+    return this.itemRepository.save(item);
+  }
+
+  async remove(id: string): Promise<Item> {
+    // Todo: soft delete, integrida referencial
+    const item = await this.findOne(id);
+    await this.itemRepository.remove(item);
+
+    return { ...item, id };
+  }
+}
